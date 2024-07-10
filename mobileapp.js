dcd811388e3e3514196a3f92d54d4c02d713b504
Kojo-Jr <omarieben7@gmail.com>
2024-07-10 18:19:58 +0000

    creating sign up and login forms

---
+++ mobileapp.js
@@ -0,0 +1,9 @@
+// Loops :  used to execute a repeated task
+
+
+// for loops : how many times a system can run
+// parts of for loop(initialization, condition, inc or dec)
+
+for (let x = 0; x < 5; i++){
+    console.log(x)
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+//let x;
+//x = 100;
+
+let fullName = 'Omari';
+let age = 29
+let student = false;
+
+//console.log(x);
+document.getElementById(`p1`).innerHTML = `Your name is ${fullName}`;
+document.getElementById(`p2`).innerHTML = `You are  ${age} years old`;
+document.getElementById(`p3`).innerHTML = `You are enrolled in school: ${student}`;
